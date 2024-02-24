5f989b5a294015524eef05fa0f768d19c618bef2
GabrielFernandez1 <gabriel.fernandez1@udc.es>
2024-02-24 17:12:02 +0100

    Ejemplo funcional MC 1D & Añadido fichero versión 2D

---
+++ mc_metropolis_1D.py
@@ -0,0 +1,41 @@
+import matplotlib . pyplot as plt
+import numpy as np
+
+g1 = lambda x:2*(x[0] + x[1])
+f  = lambda x:np.sin(x)/2
+
+a = 0
+b = np.pi
+nsim = 1550 # numero de puntos a generar
+x = []
+Ia = 0 # para aproximar la esperanza
+Naceptados = 0
+x0 = np.random.uniform(a, b)
+x.append(x0)
+for n in  range(1, nsim):
+    g = np.random.uniform(-1.2, 1.2)
+    xs = x0 + g
+    # controlo no salirme
+    if (xs < a or xs > b):
+        v = np.random.uniform(0, 1)
+        xs = a + (b - a) * v
+    c = f(xs) / f(x0)
+    A = np.minimum(1., c)
+    if A == 1: # me quedo el punto
+        x0 = xs
+        x.append(x0)
+        Naceptados += 1
+        Ia += g1(x)
+    elif A < 1.: # decido aleatoriamente que hacer
+        u = np.random.uniform(0, 1)
+        if u < A:
+            x0 = xs
+            x.append(x0)
+            Naceptados += 1
+            Ia += g1(x)
+
+Ia /= Naceptados
+print('Ia = ', Ia)
+xx = np.linspace(a, b)
+plt.plot(xx, f(xx))
+plt.plot(x, np.zeros_like(x), 'x')
