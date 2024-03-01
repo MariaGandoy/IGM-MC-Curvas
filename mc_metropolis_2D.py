2a9dad90c3b5e0bb8ab32e85a838d707e623e3e3
GabrielFernandez1 <gabriel.fernandez1@udc.es>
2024-03-01 17:40:14 +0100

    mc crudo

--- mc_metropolis_2D.py
+++ mc_metropolis_2D.py
@@ -19,6 +19,16 @@ import numpy as np
 g1  = lambda x, y, c:np.cos((x[0] + x[1]) + (y[0] + y[1])) * c
 f   = lambda x, c: np.exp(x) / c
 
+def montecarloCrudo(nsims, a, b):
+    f = lambda x: np.exp(x)
+    Ia = 0.
+
+    for n in range(1, nsmis):
+        x = np.random.uniform(a, b)
+        Ia += f(x)
+
+    return (Ia / nsims)
+
 a = [0, 0]
 b = [(np.pi / 2), 1]
 nsim = 1550 # numero de puntos a generar
@@ -38,8 +48,10 @@ for n in range(1, nsim):
     # controlo no salirme
     if (xs < a[0] or xs > b[0] or ys < a[1] or ys > b[1]):
         v = [np.random.uniform(0, 1), np.random.uniform(0, 1)]
-        xs = a + (b - a) * v
-    mc = montecarloCrudo()
+        xs = a[0] + (b[0] - a[0]) * v[0]
+        ys = a[1] + (b[1] - a[1]) * v[1]
+    #mc = montecarloCrudo()
+    mc = montecarloCrudo(nsim, a[0], b[0])
     c = [f(xs, mc) / f(x0, mc), f(ys, mc) / f(x0, mc)]
     A = np.minimum(1., c)
     if A[0] == 1 and A[1] == 1:
@@ -58,6 +70,7 @@ for n in range(1, nsim):
             y.append(y0)
             Naceptados += 1
             Ia += g1(x, y, mc)
+
 Ia /= Naceptados
 print('Ia = ', Ia)
 # xx = np.linspace(a, b)
@@ -77,8 +90,19 @@ print('Ia = ', Ia)
 #           integral[0, pi/2] integral[0, 1] (cos(x + y) / c) dx dy = 1
 #           integral[0, pi/2] integral[0, 1] (f(x, y) / c) dx dy = 1 , f(x, y) = cos(x + y)
 
-g1  = lambda x, y, c:np.exp(x[0] + x[1]) * c
+g1  = lambda x, c:np.exp(x[0] + x[1]) * c
 f   = lambda x, y, c: np.cos(x, y) / c
+
+def montecarloCrudo(nsims, a1, b1, a2, b2):
+    f = lambda x, y: np.cos(x + y)
+    Ia = 0.
+
+    for n in range(1, nsmis):
+        x = np.random.uniform(a1, b1)
+        x = np.random.uniform(a2, b2)
+        Ia += f(x, y)
+
+    return (Ia / nsims)
 
 a = [0, 0]
 b = [(np.pi / 2), 1]
@@ -99,10 +123,16 @@ for n in range(1, nsim):
     # controlo no salirme
     if (xs < a[0] or xs > b[0] or ys < a[1] or ys > b[1]):
         v = [np.random.uniform(0, 1), np.random.uniform(0, 1)]
-        xs = a + (b - a) * v
-    mc = montecarloCrudo()
-    c = [f(xs, mc) / f(x0, mc), f(ys, mc) / f(x0, mc)]
-    A = np.minimum(1., c)
+        xs = a[0] + (b[0] - a[0]) * v[0]
+        ys = a[1] + (b[1] - a[1]) * v[1]
+    #mc = montecarloCrudo()
+    mc = montecarloCrudo(nsim, a[0], b[0], a[1], b[1])
+    f1= f(xs, ys,  mc) / f(x0, y0, mc)
+    f2= f(ys, xs, mc) / f(y0, x0, mc)
+    c = [f1, f2]
+    A = [0., 0.]
+    A[0] = np.minimum(1., c[0])
+    A[1] = np.minimum(1., c[1])
     if A[0] == 1 and A[1] == 1:
         x0 = xs
         y0 = ys
@@ -119,6 +149,7 @@ for n in range(1, nsim):
             y.append(y0)
             Naceptados += 1
             Ia += g1(x, y, mc)
+
 Ia /= Naceptados
 print('Ia = ', Ia)
 # xx = np.linspace(a, b)
